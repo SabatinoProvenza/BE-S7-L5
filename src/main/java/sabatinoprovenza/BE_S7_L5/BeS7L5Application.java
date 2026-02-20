03080842339bf934a994967aa23e1880e9f05c89
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-02-20 09:33:51 +0100

    Initial commit

---
+++ src/main/java/sabatinoprovenza/BE_S7_L5/BeS7L5Application.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BE_S7_L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/sabatinoprovenza/BE_S7_L5/BeS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BE_S7_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
